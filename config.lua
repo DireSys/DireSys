24deed971778b5f04acc8e6696467926e4f33c77
Benjamin Zaporzan <benzaporzan@gmail.com>
2016-10-07 12:22:57 -0400

    reduced physics scaling, less stuck areas between walls

--- config.lua
+++ config.lua
@@ -10,6 +10,6 @@ config.WINDOW_SCALE = 4
 config.TILE_SIZE = 4
 config.SPRITESHEET_FILENAME = "assets/SpriteSheet-1.png"
 
-config.PHYSICS_SCALE = 16 -- 16px is 1 meter
+config.PHYSICS_SCALE = 2
 
 return config
