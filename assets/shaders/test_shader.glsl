42f5dadddcb61e2a4f92604afdda4f1178cb6840
Benjamin Zaporzan <benzaporzan@gmail.com>
2016-10-12 00:10:21 -0400

    basic shadow casting working

--- assets/shaders/test_shader.glsl
+++ assets/shaders/test_shader.glsl
@@ -12,6 +12,9 @@ parameter ivec2 blockLocation = ivec2(8, 8) : range(ivec2(0, 0), ivec2(31, 31));
 parameter float lightMaxDistance = 32.0 : range(0.0, 512.0);
 parameter float lightMinFalloff = 16.0 : range(0.0, 512.0);
 
+// Other Constants
+const float EPSILON = 2.0;
+
 // Window Size
 const int TILE_SIZE = 16;
 const int WINDOW_WIDTH = 512;
@@ -27,25 +30,96 @@ glsl vec4 toFloatColor(in vec4 c) {
     return c/255.0;
 }
 
+glsl vec2 TO_TILE(vec2 pixelPosition) {
+    return vec2(floor(pixelPosition.x / TILE_SIZE),
+		floor(pixelPosition.y / TILE_SIZE));
+}
+
+glsl vec2 TO_WORLD(vec2 tilePosition) {
+    return vec2(floor(tilePosition.x * TILE_SIZE),
+		floor(tilePosition.y * TILE_SIZE));
+}
+
+// Block Boundary Functions
+glsl vec2 tile_getTopLeft(vec2 pixelPosition) {
+    vec2 tilePosition = TO_TILE(pixelPosition);
+    tilePosition.y += 1;
+    return TO_WORLD(tilePosition);
+}
+
+glsl vec2 tile_getTopRight(vec2 pixelPosition) {
+    vec2 tilePosition = TO_TILE(pixelPosition);
+    tilePosition.x += 1;
+    tilePosition.y += 1;
+    return TO_WORLD(tilePosition);
+}
+
+glsl vec2 tile_getBottomLeft(vec2 pixelPosition) {
+    vec2 tilePosition = TO_TILE(pixelPosition);
+    return TO_WORLD(tilePosition);
+}
+
+glsl vec2 tile_getBottomRight(vec2 pixelPosition) {
+    vec2 tilePosition = TO_TILE(pixelPosition);
+    tilePosition.x += 1;
+    return TO_WORLD(tilePosition);
+}
+
+glsl float getLightDistance(vec2 pixelPosition) {
+    return distance(lightSource, pixelPosition);
+}
+
+glsl float getMaxLightDistance(vec2 pixelPosition) {
+    return max(0,
+	       max(getLightDistance(tile_getBottomLeft(pixelPosition)),
+		   max(getLightDistance(tile_getBottomRight(pixelPosition)),
+		       max(getLightDistance(tile_getTopLeft(pixelPosition)),
+			   getLightDistance(tile_getTopRight(pixelPosition))))));
+}
+
+glsl float getMinLightDistance(vec2 pixelPosition) {
+    return min(getLightDistance(tile_getBottomLeft(pixelPosition)),
+	       min(getLightDistance(tile_getBottomRight(pixelPosition)),
+		   min(getLightDistance(tile_getTopLeft(pixelPosition)),
+		       getLightDistance(tile_getTopRight(pixelPosition)))));
+}
+
+glsl bool isInRange(vec2 pixelPosition) {
+    float lightDistance = getLightDistance(pixelPosition);
+    return lightDistance < lightMaxDistance;
+}
+
+
+
+glsl vec4 getLightIntensity(vec2 pixelPosition) {
+    float lightDistance = getMinLightDistance(pixelPosition);
+    if (lightDistance < lightMinFalloff) {
+	return toFloatColor(WHITE);
+    }
+    else {
+	return toFloatColor(GREY);
+    }
+}
+
+glsl vec4 setShadowCast(vec2 pixelPosition) {
+    if (getMaxLightDistance(pixelPosition) < lightMaxDistance) {
+	return getLightIntensity(pixelPosition);
+    }
+    return toFloatColor(BLACK);
+}
+
 glsl vec4 shadows(vec2 position) {
-    vec2 pixelPosition = ivec2(floor(WINDOW_WIDTH * position.x),
-			       floor(WINDOW_HEIGHT * position.y));
+    vec2 pixelPosition = vec2(floor(WINDOW_WIDTH * position.x),
+			      floor(WINDOW_HEIGHT * position.y));
 
-    vec2 tilePosition = ivec2(pixelPosition.x / TILE_SIZE,
-			      pixelPosition.y / TILE_SIZE);
+    vec2 tilePosition = TO_TILE(pixelPosition);
 
-    // tiles are defined by the top-left corner
-    if (tilePosition.x == blockLocation.x && tilePosition.y == blockLocation.y) {
+    // tiles are defined by the bottom-left corner
+    if (tilePosition == blockLocation) {
 	return toFloatColor(BLACK);
     }
 
-    // start working with shadows
-    float lightDistance = distance(lightSource, pixelPosition);
-    if (lightDistance < lightMaxDistance) {
-	return toFloatColor(GREY);
-    }
-
-    return toFloatColor(WHITE);
+    return setShadowCast(pixelPosition);
 }
 
 glsl vec4 dither(vec4 x) {
